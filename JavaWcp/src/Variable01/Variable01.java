f53a8821f23b089d37dc838cd94d45d63ae5670d
Masaki Yoshimura <masakiyoshimura@MasakinoMacBook-Pro.local>
2021-06-04 12:19:22 +0900

    first commit

---
+++ JavaWcp/src/Variable01/Variable01.java
@@ -0,0 +1,13 @@
+package Variable01;
+
+public class Variable01 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 27;
+		String str1;
+		str1 = "Masaki Yoshimura";
+		System.out.println(str1);
+		str1 = "hoge";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
