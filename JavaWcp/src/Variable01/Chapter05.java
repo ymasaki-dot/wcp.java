d4a682e626a7c8b7b2c766c4217c40fa54bde8bf
Masaki Yoshimura <masakiyoshimura@MasakinoMacBook-Pro.local>
2021-06-04 18:35:40 +0900

    5章終了

---
+++ JavaWcp/src/Variable01/Chapter05.java
@@ -0,0 +1,22 @@
+package Variable01;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 1000000L;
+		System.out.println(l);
+		double d = 3.14;
+		System.out.println(d);
+		boolean b = true;
+		System.out.print(b);
+		
+		Integer i = 100;
+		System.out.println(i);
+		Float f = 1.221f;
+		System.out.println(f);
+		Character c = '魔';
+		System.out.println(c);
+		
+		int[] array = {0,100,150,6,70,};
+		System.out.println(array[3]);
+	}
+}
---
+++ JavaWcp/src/Variable01/Null01.java
@@ -0,0 +1,16 @@
+package Variable01;
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		string1 = "あいう";
+		System.out.println(string1 + "の文字数:" + string1.length());
+		string1 = "";
+		System.out.println(string1 + "の文字数:" + string1.length());
+		
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[1] = 20;
+		array1[2] = 20;
+	}
+}
