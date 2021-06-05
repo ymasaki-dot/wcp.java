3727e4e13666acc45251e14560ed1db56078d78a
ymasaki-dot <y.masaki0526@gmail.com>
2021-06-05 15:38:52 +0900

    7章終了

---
+++ JavaWcp/src/Variable01/IF01.java
@@ -0,0 +1,16 @@
+package Variable01;
+
+public class IF01 {
+	public static void main(String[] args) {
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + " は5未満です");
+		}
+		if (number < 10) {
+			System.out.println(number + " は5以上です");
+		}
+		if (number < 15) {
+			System.out.println(number + " は15未満です");
+		}
+	}
+}
---
+++ JavaWcp/src/Variable01/If02.java
@@ -0,0 +1,17 @@
+package Variable01;
+
+public class If02 {
+	public static void main(String[] args) {
+		int val = 2;
+		switch (val) {
+		case 3:
+			System.out.println("よくできました！");
+			break;
+		case 2:
+			System.out.println("普通でした");
+			break;
+		default:
+			System.out.println("頑張ろう！");
+		}
+	}
+}
