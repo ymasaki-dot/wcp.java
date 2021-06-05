459927ae9aa62e6b19a9c1209f0bba0e2b1455ac
ymasaki-dot <y.masaki0526@gmail.com>
2021-06-05 16:30:42 +0900

    7章課題終了

--- JavaWcp/src/Variable01/chapter07.java
+++ JavaWcp/src/Variable01/chapter07.java
@@ -1,5 +1,31 @@
 package Variable01;
 
 public class chapter07 {
-
+	public static void main(String[] args) {
+		int distance = 15;
+		if (distance <= 5) {
+			System.out.println("とても近いです");			
+		} else if (distance <= 10) {
+			System.out.println("近いです");
+		} else if (distance <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+		
+		String color = "yelow";
+		switch(color) {
+		case "red" :
+			System.out.println("赤信号です");
+			break;
+		case "yellow" :
+			System.out.println("黄色信号です");
+			break;
+		case "blue" :
+			System.out.println("青信号です");
+			break;
+		default :
+			System.out.println("信号の色ではないです");
+		}
+	}
 }
