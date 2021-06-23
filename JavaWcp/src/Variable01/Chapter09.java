db735b9280ee1dd023ae973fe809c6ced26234f5
ymasaki-dot <y.masaki0526@gmail.com>
2021-06-23 10:19:38 +0900

    update 10章

--- JavaWcp/src/Variable01/Chapter09.java
+++ JavaWcp/src/Variable01/Chapter09.java
@@ -1,5 +1,12 @@
 package Variable01;
 
 public class Chapter09 {
-
+	public static void main(String[] args) {
+		double fuelCost = 12.5;
+		double fuelAmount = 55.0;
+		Car car = new Car(fuelCost, fuelAmount);
+		
+		car.move(20);
+		System.out.println("残量は、" + car.getFuelAmount() + "です");		
+	}
 }
--- JavaWcp/src/Variable01/Exception01.java
+++ JavaWcp/src/Variable01/Exception01.java
@@ -1,5 +1,10 @@
 package Variable01;
 
 public class Exception01 {
-
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0 は？" );
+		int result = 100 / 0;
+		System.out.println("計算結果" + result);
+		System.out.println("プログラム終了");		
+	}
 }
