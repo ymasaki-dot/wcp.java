1d2e1181358b3fbb732a760d8305a15f3b011590
ymasaki-dot <y.masaki0526@gmail.com>
2021-06-24 10:39:45 +0900

    11章終了

--- JavaWcp/src/Variable01/HumanCapsule.java
+++ JavaWcp/src/Variable01/HumanCapsule.java
@@ -1,5 +1,24 @@
 package Variable01;
 
 public class HumanCapsule {
+	private String name;
+	private int age;
+	
+	public HumanCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
 
 }
--- JavaWcp/src/Variable01/HumanNoCapsule.java
+++ JavaWcp/src/Variable01/HumanNoCapsule.java
@@ -1,5 +1,11 @@
 package Variable01;
 
 public class HumanNoCapsule {
-
+	public String name;
+	public int age;
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
